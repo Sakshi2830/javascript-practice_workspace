163cdef474693fb78f949ee8e8b59b050c8e081a
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-04-29 23:08:22 +0530

    switch commit

---
+++ jsbasic/switch.js
@@ -0,0 +1,25 @@
+//Create an appliction with following roles..
+//admin - gets full access
+//subadmin - gets access to create/delete contents
+//testprep - gets access to create/delete prepared tests
+//user - gets access to consume content
+  
+var person = "testprep";
+
+switch (person) {
+    case "admin":
+        console.log("gets full access");
+        break;
+    case "subadmin":
+        console.log("gets access to create/delete contents");
+        break;    
+    case "testprep":
+        console.log("gets access to create/delete prepared tests");
+        break;
+    case "user":
+        console.log("gets access to consume content");
+        break;
+    default:
+        console.log("trial user");
+        break;
+}
