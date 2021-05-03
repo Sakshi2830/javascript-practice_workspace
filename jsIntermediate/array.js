d6c01a396ce651b915f5d8abd0cc4ebdd0ed2d7d
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-05-03 22:00:28 +0530

    more conepts in array

--- jsIntermediate/array.js
+++ jsIntermediate/array.js
@@ -8,7 +8,7 @@ console.log(states[0]);
 states[0] = "ujjain"
 
 
-console.log(states)
+console.log(states)// o/p =>  ['ujjain', 'Rajasthan', 'Assam', 'Punjab' ]
 
 console.log(countries.concat(states));
 // o/p =>[
@@ -25,3 +25,16 @@ console.log(countries); // o/p => [ 'India', 'USA', 'Japan', 'Vietnam', 'China' 
 
 countries.pop();
 console.log(countries);// o/p => [ 'India', 'USA', 'Japan', 'Vietnam' ]
+
+
+
+// PART - 2...........................
+var isEven = (element) =>{
+    return element % 2 == 0;
+};
+console.log(isEven(3)); //o/p => false
+
+
+var result = ['2', '4', '6', '8'].every((e) => (e % 2 === 0));
+console.log(result);
+// o/p => true
