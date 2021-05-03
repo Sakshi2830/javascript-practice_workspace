d470aed21faec8b2e2da6afb1830b836f644cbb5
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-05-03 22:33:13 +0530

    fill and filter concept

--- jsIntermediate/array.js
+++ jsIntermediate/array.js
@@ -38,3 +38,20 @@ console.log(isEven(3)); //o/p => false
 var result = ['2', '4', '6', '8'].every((e) => (e % 2 === 0));
 console.log(result);
 // o/p => true
+
+
+//fill and filter method in Array.......................
+
+
+var testArray = [2,3,4,5,6,7,8,9,22];
+console.log(testArray.fill("empty"))//fill every value with empty keyword..
+
+console.log("updated value=> " +  testArray.fill("empty", 3,7));
+//o/p => updated value=> 2,3,4,empty,empty,empty,empty,9,22
+
+const myNum = [23,45,67,89,21,54,87];
+
+const result = myNum.filter((num) => num > 54);
+
+console.log(result);
+// o/p => [ 67, 89, 87 ]
