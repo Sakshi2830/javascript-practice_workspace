857a98e097bdcd02696f3a83ec7b849e973cbb45
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-05-05 00:14:28 +0530

    some more types of loop

--- jsIntermediate/loops.js
+++ jsIntermediate/loops.js
@@ -32,3 +32,47 @@ let i=0;
     i++;
     
 }while (i < myStates.length)
+
+
+//--------------FOR EACH LOOP-----------
+
+
+myStates.forEach((s) => (console.log(s)));
+//o/p => 
+/*Rajasthan
+Up   
+Delhi
+1947 
+Assam
+Tamil Nadu
+Maharastra*/
+
+//-----------------FOR OF AND FOR IN-----------------
+var sites = ["youTube", "facebook", "twitter", "instagram"];
+
+for(const n of sites ){
+    console.log(n);
+}//o/p =>
+/*youTube
+facebook
+twitter
+instagram*/
+
+var symbols ={
+    yt: "youtube",
+    ig: "instagram",
+    fb: "facebook",
+    com: "commerce"
+}
+
+for(const n in symbols){
+    // console.log(n);//o/p =>
+/*yt
+ig 
+fb 
+com*/
+    console.log(`key is: ${n} value is: ${symbols[n]}`);}//o/p => 
+/*key is: yt value is: youtube
+key is: ig value is: instagram
+key is: fb value is: facebook
+key is: com value is: commerce*/
